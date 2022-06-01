87c8b47d2a44f68eb1226363fae388ca98115b37
Dmitry Zavalishin <dz@dz.ru>
2022-06-02 02:15:12 +0300

    parse midi

---
+++ src/ru/dz/bluearp/BlueBroomMain.java
@@ -0,0 +1,159 @@
+package ru.dz.bluearp;
+
+import java.io.File;
+
+import javax.sound.midi.MetaMessage;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+
+public class BlueBroomMain {
+
+
+
+
+	//private static final String MIDI_FILE = "G:\\Projects\\BroomStickExtractor\\midi\\Disco_Octaves.mid";
+	private static final String MIDI_FILE = "midi/Disco_Octaves.mid";
+	//private static final String MIDI_FILE = "midi/Classic_Disco.mid"; 
+	
+	public static final int NOTE_ON = 0x90;
+	public static final int NOTE_OFF = 0x80;
+	public static final String[] NOTE_NAMES = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+
+	public static void main(String[] args) throws Exception {
+		Sequence sequence = MidiSystem.getSequence(new File(MIDI_FILE));
+
+		int trackNumber = 0;
+		for (Track track :  sequence.getTracks()) 
+		{
+			trackNumber++;
+			printMidiTrack(trackNumber, track);
+		}
+
+	}
+
+	private static void printMidiTrack(int trackNumber, Track track) {
+		System.out.println("Track " + trackNumber + ": size = " + track.size());
+		System.out.println();
+		for (int i=0; i < track.size(); i++) 
+		{ 
+			MidiEvent event = track.get(i);
+			printMidiEvent(event);
+		}
+
+		System.out.println();
+	}
+
+	private static void printMidiEvent(MidiEvent event) {
+		//System.out.print("@" + event.getTick() + " ");
+		MidiMessage message = event.getMessage();
+		if (message instanceof ShortMessage) {
+			ShortMessage sm = (ShortMessage) message;
+			//System.out.print("Channel: " + sm.getChannel() + " ");
+			if (sm.getCommand() == NOTE_ON) {
+				//printMidiNote(sm,"on");
+				printMidiNoteShort(sm);
+			} else if (sm.getCommand() == NOTE_OFF) {
+				printMidiNote(sm,"off");
+			} else {
+				System.out.println("Command:" + sm.getCommand());
+			}
+		} else if (message instanceof MetaMessage) { 
+			MetaMessage mm = (MetaMessage) message;
+			printMidiMeta(mm);
+		} 
+		else {
+			System.out.println("Other message: " + message.getClass()+" = '"+message.toString()+"'");
+		}
+	}
+
+
+	private static void printMidiMeta(MetaMessage mm) 
+	{
+		final byte[] b = mm.getData();
+		
+		System.out.print("Meta message type: " + mm.getType()+" ");
+		switch(mm.getType())
+		{
+		case 47:	System.out.print("(stop)"); break;
+		case 3:		System.out.print("(track name '"+getString(mm)+"')"); break;
+		
+		case 0x51:	
+			long bpm = 60 * 1000 * 1000 / getInt3(mm);
+			System.out.print("(tempo "+bpm+" BPM)"); 
+			break;
+		
+		case 0x58:	
+			int num = b[0];
+			int den = 1 << b[1];
+			int ticks = b[2];
+			int n32 = b[3];
+			System.out.print("(signature "+num+"/"+den+", "); 
+			System.out.print(""+ticks+" ticks/beat, "+n32+"/32 per beat)"); 
+			break;
+		}
+		
+		System.out.println();
+		
+		/* TODO
+		 * 
+		Message				Meta type	Data length	Contains	Occurs at
+		Sequence number		0x00	2 bytes		The number of a sequence											At delta time 0
+		Text				0x01	variable	Some text															Anywhere
+		Copyright notice	0x02	variable	A copyright notice						At delta time 0 in the first track
+		Track name			0x03	variable	A track name												At delta time 0
+		Instrument name		0x04	variable	The name of an instrument in the current track						Anywhere
+		Lyrics				0x05	variable	Lyrics, usually a syllable per quarter note							Anywhere
+		Marker				0x06	variable	The text of a marker												Anywhere
+		Cue point			0x07	variable	The text of a cue, usually to prompt for some action from the user	Anywhere
+		Channel prefix		0x20	1 byte		A channel number (following meta events will apply to this channel)	Anywhere
+		End of track		0x2F	0	 														At the end of each track
+		Set tempo			0x51	3			The number of microseconds per beat				Anywhere, but usually in the first track
+		SMPTE offset		0x54	5			SMPTE time to denote playback offset from the beginning				Anywhere
+		Time signature		0x58	4			Time signature, metronome clicks, and size of a beat in 32nd notes	Anywhere
+		Key signature		0x59	2			A key signature														Anywhere
+		Sequencer specific	0x7F	variable	Something specific to the MIDI device manufacturer					Anywhere		 
+		* 
+		*/
+	}
+
+	private static int getInt3(MetaMessage mm) 
+	{
+		int r = 0;
+		
+		byte[] b = mm.getData();
+		
+		r |= ((int)b[2]) & 0xFF;
+		r |= (((int)b[1]) & 0xFF) << 8;
+		r |= (((int)b[0]) & 0xFF) << 16;
+		
+		return r;
+	}
+
+	private static String getString(MetaMessage mm) {		
+		return new String(mm.getData());
+	}
+
+	private static void printMidiNote(ShortMessage sm, String msg) 
+	{
+		int key = sm.getData1();
+		int octave = (key / 12)-1;
+		int note = key % 12;
+		String noteName = NOTE_NAMES[note];
+		int velocity = sm.getData2();
+		System.out.println("Note "+msg+", " + noteName + octave + " key=" + key + " velocity: " + velocity);
+	}
+
+	private static void printMidiNoteShort(ShortMessage sm) 
+	{
+		int key = sm.getData1();
+		int octave = (key / 12)-1;
+		int note = key % 12;
+		String noteName = NOTE_NAMES[note];
+	
+		System.out.print(noteName + octave + " ");
+	}
+}	
---
+++ src/ru/dz/bluearp/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author dz
+ *
+ * Basic attempt to parse MIDI (created by BroomStickExtractor) and generate
+ * BlueArp arpeggio patterns.
+ *
+ */
+package ru.dz.bluearp;
